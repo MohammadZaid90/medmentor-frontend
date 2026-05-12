02105593986d17b89f64a24f9c3b389b83aa6b05
MohammadZaid90 <sheikhzaid768@gmail.com>
2026-05-12 07:13:00 +0500

    fix render preview allowedHosts issue

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      "medmentor-frontend.onrender.com"
+    ]
+  }
 })
